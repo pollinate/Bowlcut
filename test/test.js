b17c33379553d7affdf82d9a078af7f36b452c60
Duncan Alexander <hypo@hypothete.com>
2016-04-20 13:00:54 -0700

    clean up test

--- test/test.js
+++ test/test.js
@@ -2,20 +2,20 @@
 
 'use strict';
 
-var //centerSVG = //document.querySelector('#center-test'),
-	//reverseSVG = document.querySelector('#reverse-test'),
+var centerSVG = document.querySelector('#center-test'),
+	reverseSVG = document.querySelector('#reverse-test'),
 	circleSVG = document.querySelector('#circle-test'),
 	textInput = document.querySelector('input'),
 	glyphBehaviorSelect = document.querySelector('#glyphbehavior-select'),
 	minBehaviorSelect = document.querySelector('#minbehavior-select'),
 	maxBehaviorSelect = document.querySelector('#maxbehavior-select'),
-	//centerCut = new Bowlcut(),
-	//reverseCut = new Bowlcut(),
+	centerCut = new Bowlcut(),
+	reverseCut = new Bowlcut(),
 	circleCut = new Bowlcut(),
-	textPaths = [circleCut]; //centerCut, reverseCut, 
+	textPaths = [circleCut, centerCut, reverseCut]; 
 
-//centerCut.set.path(centerSVG.querySelector('path'), false);
-//reverseCut.set.path(reverseSVG.querySelector('path'), true);
+centerCut.set.path(centerSVG.querySelector('path'), false);
+reverseCut.set.path(reverseSVG.querySelector('path'), true);
 circleCut.set.pathFromArc(250,300,200,70,'up');
 circleSVG.appendChild(circleCut.path);
 
@@ -42,14 +42,14 @@ function redraw(){
 		.set.maxBehavior(maxBehaviorSelect.value)
 		.set.glyphBehavior(glyphBehaviorSelect.value);
 		var textToAppend = bc.render();
-		/*if(bc.uniqueId === centerCut.uniqueId){
+		if(bc.uniqueId === centerCut.uniqueId){
 			centerSVG.appendChild(textToAppend);
 		}
 		else if(bc.uniqueId === reverseCut.uniqueId){
 			reverseSVG.appendChild(textToAppend);
 		}
-		else if(bc.uniqueId === circleCut.uniqueId){*/
+		else if(bc.uniqueId === circleCut.uniqueId){
 			circleSVG.appendChild(textToAppend);
-		//}
+		}
 	});
 }
